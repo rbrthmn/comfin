9502bc4176ddfdbfb2c2ca12159a2e608f2cd0c9
rbrthmn <robertokenzohamano@gmail.com>
2025-02-21 16:44:48 -0300

    deleting comment

--- app/src/main/java/br/com/rbrthmn/di/AppModule.kt
+++ app/src/main/java/br/com/rbrthmn/di/AppModule.kt
@@ -53,9 +53,6 @@ val appModule = module {
     viewModelOf<BalanceCardContract.BalanceCardViewModel>(::BalanceCardViewModel)
     viewModelOf<CreditCardBillsCardContract.CreditCardsBillCardViewModel>(::CreditCardBillsCardViewModel)
     viewModelOf<LastMonthDifferenceCardContract.LastMonthDifferenceCardViewModel>(::LastMonthDifferenceCardViewModel)
-//    viewModel<LastMonthDifferenceCardContract.LastMonthDifferenceCardViewModel> {
-//        LastMonthDifferenceCardViewModel().onInit(LastMonthDifferenceCardUiState())
-//    }
     viewModelOf<MonthlyLimitCardContract.MonthlyLimitCardViewModel>(::MonthlyLimitCardViewModel)
     viewModel<OperationsScreenContract.OperationsScreenViewModel> {
         OperationsScreenViewModel(stringProvider = get())
