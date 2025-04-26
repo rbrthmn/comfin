8ded6bf0b40f96c62be79c8c5b1c7ce89ef2ccd4
rbrthmn <robertokenzohamano@gmail.com>
2025-04-26 17:31:48 -0300

    simplifying base view model and contract names

--- app/src/main/java/br/com/rbrthmn/di/AppModule.kt
+++ app/src/main/java/br/com/rbrthmn/di/AppModule.kt
@@ -23,12 +23,8 @@ package br.com.rbrthmn.di
 import br.com.rbrthmn.ui.financialcompanion.screens.home.HomeScreenContract
 import br.com.rbrthmn.ui.financialcompanion.screens.home.HomeScreenViewModel
 import br.com.rbrthmn.ui.financialcompanion.screens.home.components.balancecard.BalanceCardContract
-import br.com.rbrthmn.ui.financialcompanion.screens.home.components.creditcardbillscard.CreditCardBillsCardContract
-import br.com.rbrthmn.ui.financialcompanion.utils.DecimalFormatter
-import br.com.rbrthmn.ui.financialcompanion.utils.DecimalInputFieldFormatter
-import br.com.rbrthmn.ui.financialcompanion.utils.SnackBarProvider
-import br.com.rbrthmn.ui.financialcompanion.utils.SnackBarProviderImpl
 import br.com.rbrthmn.ui.financialcompanion.screens.home.components.balancecard.BalanceCardViewModel
+import br.com.rbrthmn.ui.financialcompanion.screens.home.components.creditcardbillscard.CreditCardBillsCardContract
 import br.com.rbrthmn.ui.financialcompanion.screens.home.components.creditcardbillscard.CreditCardBillsCardViewModel
 import br.com.rbrthmn.ui.financialcompanion.screens.home.components.lastmonthdifferencecard.LastMonthDifferenceCardContract
 import br.com.rbrthmn.ui.financialcompanion.screens.home.components.lastmonthdifferencecard.LastMonthDifferenceCardViewModel
@@ -36,7 +32,11 @@ import br.com.rbrthmn.ui.financialcompanion.screens.home.components.monthlylimit
 import br.com.rbrthmn.ui.financialcompanion.screens.home.components.monthlylimitcard.MonthlyLimitCardViewModel
 import br.com.rbrthmn.ui.financialcompanion.screens.operations.OperationsScreenContract
 import br.com.rbrthmn.ui.financialcompanion.screens.operations.OperationsScreenViewModel
+import br.com.rbrthmn.ui.financialcompanion.utils.DecimalFormatter
+import br.com.rbrthmn.ui.financialcompanion.utils.DecimalInputFieldFormatter
 import br.com.rbrthmn.ui.financialcompanion.utils.ResourceStringProvider
+import br.com.rbrthmn.ui.financialcompanion.utils.SnackBarProvider
+import br.com.rbrthmn.ui.financialcompanion.utils.SnackBarProviderImpl
 import br.com.rbrthmn.ui.financialcompanion.utils.StringProvider
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -50,7 +50,7 @@ val appModule = module {
     single<StringProvider> { ResourceStringProvider(context = androidContext()) }
 
     viewModelOf<HomeScreenContract.HomeScreenViewModel>(::HomeScreenViewModel)
-    viewModel<BalanceCardContract.BalanceCardViewModel> {
+    viewModel<BalanceCardContract.ViewModel> {
         BalanceCardViewModel().doOnInit()
     }
     viewModel<CreditCardBillsCardContract.CreditCardsBillCardViewModel> {
--- app/src/main/java/br/com/rbrthmn/ui/financialcompanion/screens/home/components/balancecard/BalanceCard.kt
+++ app/src/main/java/br/com/rbrthmn/ui/financialcompanion/screens/home/components/balancecard/BalanceCard.kt
@@ -55,19 +55,19 @@ import br.com.rbrthmn.ui.financialcompanion.components.DecimalInputField
 import br.com.rbrthmn.ui.financialcompanion.screens.home.components.AddItemButton
 import br.com.rbrthmn.ui.financialcompanion.screens.home.components.BanksDropdownMenu
 import br.com.rbrthmn.ui.financialcompanion.screens.home.components.TotalValueText
-import br.com.rbrthmn.ui.financialcompanion.screens.home.components.balancecard.BalanceCardContract.BalanceCardIntent.CleanNewAccount
-import br.com.rbrthmn.ui.financialcompanion.screens.home.components.balancecard.BalanceCardContract.BalanceCardIntent.OnBankChange
-import br.com.rbrthmn.ui.financialcompanion.screens.home.components.balancecard.BalanceCardContract.BalanceCardIntent.OnDateFilterChange
-import br.com.rbrthmn.ui.financialcompanion.screens.home.components.balancecard.BalanceCardContract.BalanceCardIntent.OnDescriptionChange
-import br.com.rbrthmn.ui.financialcompanion.screens.home.components.balancecard.BalanceCardContract.BalanceCardIntent.OnInitialBalanceChange
-import br.com.rbrthmn.ui.financialcompanion.screens.home.components.balancecard.BalanceCardContract.BalanceCardIntent.OnSaveClick
+import br.com.rbrthmn.ui.financialcompanion.screens.home.components.balancecard.BalanceCardContract.Intent.CleanNewAccount
+import br.com.rbrthmn.ui.financialcompanion.screens.home.components.balancecard.BalanceCardContract.Intent.OnBankChange
+import br.com.rbrthmn.ui.financialcompanion.screens.home.components.balancecard.BalanceCardContract.Intent.OnDateFilterChange
+import br.com.rbrthmn.ui.financialcompanion.screens.home.components.balancecard.BalanceCardContract.Intent.OnDescriptionChange
+import br.com.rbrthmn.ui.financialcompanion.screens.home.components.balancecard.BalanceCardContract.Intent.OnInitialBalanceChange
+import br.com.rbrthmn.ui.financialcompanion.screens.home.components.balancecard.BalanceCardContract.Intent.OnSaveClick
 import org.koin.androidx.compose.koinViewModel
 import java.time.LocalDate
 
 @Composable
 fun BalanceCard(
     modifier: Modifier = Modifier,
-    viewModel: BalanceCardContract.BalanceCardViewModel = koinViewModel(),
+    viewModel: BalanceCardContract.ViewModel = koinViewModel(),
     currentDateFilter: LocalDate
 ) {
     val uiState by viewModel.uiState.collectAsState()
@@ -105,7 +105,7 @@ fun BalanceCard(
 private fun BalanceList(
     modifier: Modifier = Modifier,
     totalBalance: String,
-    bankAccounts: List<BankAccountBalanceUiState>,
+    bankAccounts: List<BalanceCardContract.BankAccountBalanceUiState>,
     onAddItemButtonClick: () -> Unit
 ) {
     Column(
@@ -176,7 +176,7 @@ private fun BankAccountBalanceItem(
 @Composable
 private fun AddBankAccountDialog(
     modifier: Modifier = Modifier,
-    viewModel: BalanceCardContract.BalanceCardViewModel,
+    viewModel: BalanceCardContract.ViewModel,
     onSaveButtonClick: () -> Unit,
     onCancelButtonClick: () -> Unit,
 ) {
