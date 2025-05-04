16bf08fa9df5153ea020317bca7b5641e34a7a92
rbrthmn <robertokenzohamano@gmail.com>
2025-05-04 16:36:23 -0300

    refactoring credit card bills card

--- app/src/main/java/br/com/rbrthmn/ui/financialcompanion/screens/home/components/creditcardbillscard/CreditCardBillsCardContract.kt
+++ app/src/main/java/br/com/rbrthmn/ui/financialcompanion/screens/home/components/creditcardbillscard/CreditCardBillsCardContract.kt
@@ -21,20 +21,49 @@
 package br.com.rbrthmn.ui.financialcompanion.screens.home.components.creditcardbillscard
 
 import androidx.compose.runtime.MutableState
-import androidx.lifecycle.ViewModel
+import br.com.rbrthmn.R
+import br.com.rbrthmn.ui.financialcompanion.BaseViewModel
 import kotlinx.coroutines.flow.StateFlow
 import java.time.LocalDate
 
 interface CreditCardBillsCardContract {
-    abstract class CreditCardsBillCardViewModel : ViewModel() {
-        abstract val uiState: StateFlow<CreditCardsBillCardUiState>
-        abstract fun onNewCreditCardNameChange(name: String)
-        abstract fun onNewCreditCardBillChange(bill: String)
-        abstract fun onBankChange(bankIcon: Int, bankName: String)
-        abstract fun onNewCreditCardBillDueDayChange(day: Int)
-        abstract fun onSaveClick(showDialog: MutableState<Boolean>)
-        abstract fun cleanInputs()
-        abstract fun setDateFilter(date: LocalDate)
-        abstract fun doOnInit(): CreditCardBillsCardViewModel
+    abstract class CreditCardsBillCardViewModel :
+        BaseViewModel<CreditCardsBillCardUiState, Intent>() {
+        abstract override val uiState: StateFlow<CreditCardsBillCardUiState>
+        abstract override fun doOnInit(): CreditCardBillsCardViewModel
     }
+
+    sealed class Intent {
+        data class OnNewCreditCardNameChange(val name: String) : Intent()
+        data class OnNewCreditCardBillValueChange(val bill: String) : Intent()
+        data class OnBankChange(val bankIcon: Int, val bankName: String) : Intent()
+        data class OnNewCreditCardBillDueDayChange(val day: Int) : Intent()
+        data class OnSaveClick(val showDialog: MutableState<Boolean>) : Intent()
+        data object CleanInputs : Intent()
+        data class OnDateFilterChange(val date: LocalDate) : Intent()
+    }
+
+    data class CreditCardBillUiState(
+        val name: String = "",
+        val value: String = "",
+        val bankName: String = "",
+        val bankIcon: Int = R.drawable.bank_icon,
+        val canValueBeEdited: Boolean = false,
+        val dueDay: Int = 0
+    )
+
+    data class CreditCardsBillCardUiState(
+        val totalBill: String = "",
+        val bills: List<CreditCardBillUiState> = listOf(),
+        val newCreditCardName: String = "",
+        val isNewCreditCardNameValid: Boolean = true,
+        val newCreditCardBill: String = "",
+        val isNewCreditCardBillValid: Boolean = true,
+        val newCreditCardBillDueDay: Int = 0,
+        val isNewCreditCardBillDueDayValid: Boolean = true,
+        val newCreditCardBankName: String = "",
+        val isNewCreditCardBankNameValid: Boolean = true,
+        val newCreditCardBankIcon: Int = R.drawable.bank_icon,
+        var currentDateFilter: LocalDate = LocalDate.now()
+    )
 }
