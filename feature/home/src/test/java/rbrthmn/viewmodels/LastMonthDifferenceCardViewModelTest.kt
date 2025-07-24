34aa2e7ec2591d1e9c65e43ec43a37dded51f244
Roberto Kenzo <robertano@ufg.br>
2025-07-24 12:11:54 -0300

    #27 - moves last month test component to home module

--- feature/home/src/test/java/rbrthmn/viewmodels/LastMonthDifferenceCardViewModelTest.kt
+++ feature/home/src/test/java/rbrthmn/viewmodels/LastMonthDifferenceCardViewModelTest.kt
@@ -1,31 +1,10 @@
-/*
- *
- * Copyright (C) 2022 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Modifications made by Roberto Kenzo Hamano, 2024
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
-package rbthmn.viewmodels
+package rbrthmn.viewmodels
 
 import br.com.rbrthmn.home.ui.components.lastmonthdifferencecard.LastMonthDifferenceCardContract
 import br.com.rbrthmn.home.ui.components.lastmonthdifferencecard.LastMonthDifferenceCardViewModel
-import br.com.rbrthmn.home.ui.components.lastmonthdifferencecard.LastMonthDifferenceCardViewModel.Companion.MOCK
 import br.com.rbrthmn.ui.utils.formatDouble
 import junit.framework.TestCase
-import org.junit.Assert.assertEquals
+import org.junit.Assert
 import org.junit.Test
 import java.time.LocalDate
 
@@ -37,7 +16,10 @@ class LastMonthDifferenceCardViewModelTest {
     fun `doOnInit should assign initial values`() {
         viewModel.doOnInit()
 
-        assertEquals(formatDouble(MOCK), viewModel.uiState.value.valueOfLastMonth)
+        Assert.assertEquals(
+            formatDouble(LastMonthDifferenceCardViewModel.Companion.MOCK),
+            viewModel.uiState.value.valueOfLastMonth
+        )
     }
 
     @Test
