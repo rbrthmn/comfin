b9466b2d6d6880b0bac57ce1b67b2b41e6ea6794
Roberto Kenzo <robertano@ufg.br>
2025-08-12 17:17:25 -0300

    #27 - creates settings module

---
+++ feature/settings/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "br.com.rbrthmn.settings"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 26
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "META-INF/LICENSE*"
+            excludes += "META-INF/NOTICE*"
+            excludes += "META-INF/junit-platform.properties"
+            excludes += "META-INF/junit-jupiter-*.properties"
+        }
+    }
+}
+
+dependencies {
+    implementation(project(":core:ui"))
+    implementation(project(":core:navigation"))
+
+    testImplementation(project(":core:test"))
+}
