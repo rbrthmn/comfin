2ed9336b15d2e50a2e723b8b939c5356457a7eb6
Roberto Kenzo <robertano@ufg.br>
2025-08-14 17:25:03 -0300

    #27 - creates new module

--- app/src/main/java/br/com/rbrthmn/ui/financialcompanion/screens/morefeatures/MoreFeaturesScreen.kt
+++ app/src/main/java/br/com/rbrthmn/ui/financialcompanion/screens/morefeatures/MoreFeaturesScreen.kt
@@ -43,10 +43,10 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import br.com.rbrthmn.R
+import br.com.rbrthmn.settings.SettingsDestination
 import br.com.rbrthmn.ui.financialcompanion.screens.incomedivisions.IncomeDivisionsDestination
 import br.com.rbrthmn.ui.financialcompanion.screens.recurringexpenses.RecurringExpensesDestination
 import br.com.rbrthmn.ui.financialcompanion.screens.reserves.ReservesDestination
-import br.com.rbrthmn.ui.financialcompanion.screens.settings.SettingsDestination
 
 data class FeatureLabel(val name: String, val route: String)
 
